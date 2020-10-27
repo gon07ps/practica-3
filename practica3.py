4d62f8b5f355b63653e912c74354ea9077d5151a
gon07ps <gon07ps@gmail.com>
2020-10-27 18:53:01 +0100

    Version 5 Hola Mundo (Visual Studio) mensaje de agradecimiento

--- practica3.py
+++ practica3.py
@@ -1,4 +1,5 @@
 #cambio detectado en visual studio
 input("Pulsa intro para escribir Hola Mundo automaticamente")
 print("Hola Mundo")
+print("Gracias por ejecutar el programa")
 input("Pulsa intro para salir del programa")
