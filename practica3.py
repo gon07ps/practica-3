42072d35d8b0ab5529013efaeec5837413a529a5
gon07ps <gon07ps@gmail.com>
2020-10-27 18:54:41 +0100

    Version 6 Hola Mundo (Visual Studio) comentario final

--- practica3.py
+++ practica3.py
@@ -3,3 +3,4 @@ input("Pulsa intro para escribir Hola Mundo automaticamente")
 print("Hola Mundo")
 print("Gracias por ejecutar el programa")
 input("Pulsa intro para salir del programa")
+#comentario final para terminal el segundo ejemplo de la practica
